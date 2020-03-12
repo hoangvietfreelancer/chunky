2e220e9a201deb806d21142f8bdd54a929069338
viet1pro <29736874+viet1pro@users.noreply.github.com>
2020-03-12 23:12:32 +0700

    Upload

--- slave/rootfs/plugins/dynmap/web/standalone/MySQL_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbname = 'dynmap';
-$dbhost = 'style2paints.duckdns.org';
-$dbport = 3306;
-$dbuserid = 'minecraftServer';
-$dbpassword = 'p&B8&U^!%$rRNESM';
-$dbprefix = '';
-$loginenabled = true;
-?>
